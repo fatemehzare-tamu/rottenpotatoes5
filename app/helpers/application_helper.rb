3a2237ef1b63b92f1917c5bb08941265d0e434e4
fatemehzare-tamu <fatemehzare@tamue.edu>
2024-10-09 09:53:36 -0500

    T4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
+# module ApplicationHelper
+# end
+
+
 module ApplicationHelper
+  def sort_indicator(column, current_sort, current_direction)
+    return "" unless column == current_sort
+
+    if current_direction == 'asc'
+      '↑'  # Up arrow for ascending
+    else
+      '↓'  # Down arrow for descending
+    end
+  end
 end
